53cafbef4a80b7b1422715a430df5d7f9ac6fd5f
VivekJung <vivekjunghamal@gmail.com>
2022-06-20 17:52:23 +0545

    LOGIC- handle app api response

---
+++ lib/data/response/api_response.dart
@@ -0,0 +1,24 @@
+import 'package:mvm_solid_provider/data/response/status.dart';
+
+//this is handling response
+
+class ApiResponse<T> {
+  Status? status;
+  T? data;
+  String? message;
+
+  ApiResponse(this.status, this.data, this.message);
+
+  //assigning what is happening  i.e. if it is loading then pick the status as loading and so on
+
+  ApiResponse.loading() : status = Status.loading;
+
+  ApiResponse.completed() : status = Status.completed;
+
+  ApiResponse.error() : status = Status.error;
+
+  @override
+  String toString() {
+    return "Status : $status \n Message: $message \n Data: $data";
+  }
+}
---
+++ lib/data/response/status.dart
@@ -0,0 +1,4 @@
+// enum is used for showing enumerated data types
+// enumeration: mentioning number of things one by one
+// each data member is assigned in incremental order starting with 0
+enum Status { loading, completed, error }
